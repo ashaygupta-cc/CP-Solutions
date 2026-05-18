43ada683bce06dd50381fdbd427826c4240265f5
LeetCode <leetcode@users.noreply.github.com>
2026-05-18 12:51:59 +0530

    [LC] Counting Words With a Given Prefix [Array]

---
+++ LeetCode/Topic_Wise/Array/Counting_Words_With_a_Given_Prefix.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int prefixCount(vector<string>& words, string pref) {
+        int count = 0;
+
+        for (int i = 0; i < words.size(); i++) {
+            if (pref == words[i].substr(0, pref.size())) {
+                count++;
+            }
+        }
+        return count;
+    }
+};
