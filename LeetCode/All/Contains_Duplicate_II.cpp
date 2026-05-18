db36fbaf18c30b5de71fa1cbad99afdf99437ab9
LeetCode <leetcode@users.noreply.github.com>
2026-05-18 12:56:05 +0530

    [LC] Contains Duplicate II [Array]

---
+++ LeetCode/All/Contains_Duplicate_II.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_set<int> seen;
+        int n = nums.size();
+        
+        for (int i = 0; i < n; i++) {
+            if (seen.count(nums[i])) {
+                return true;
+            }
+            seen.insert(nums[i]);
+            
+            if (seen.size() > k) {
+                seen.erase(nums[i - k]);
+            }
+        }
+        
+        return false;
+    }
+};
