12ed39451580f62a6fa21353cf2a043127747425
LeetCode <leetcode@users.noreply.github.com>
2026-05-18 12:55:14 +0530

    [LC] Roman to Integer [Hash Table]

---
+++ LeetCode/All/Roman_to_Integer.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+    unordered_map<char,int>ROMAN = {{'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},   {'M',1000}};
+  
+    int n = s.size();
+    int num = 0;
+    
+    for(int i=0 ; i<n ; i++){
+        if(i<n-1 && ROMAN[s[i]]< ROMAN[s[i+1]]){
+            num -= ROMAN[s[i]];
+        }
+        else {
+            num += ROMAN[s[i]];
+        }
+    }
+    return num;
+}
+};
